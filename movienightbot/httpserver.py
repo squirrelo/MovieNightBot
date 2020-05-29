7708cd20aaf9c53fc1880957568936b9b15cfe95
Joshua Shorenstein <jshorens@gmail.com>
2020-05-28 21:10:34 -0600

    static file serving from bot

--- movienightbot/httpserver.py
+++ movienightbot/httpserver.py
@@ -1,4 +1,5 @@
 from typing import Dict, Any
+from pathlib import Path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 import logging
@@ -12,11 +13,14 @@ logger = logging.getLogger("movienightbot")
 
 
 class BotRequestHandler(BaseHTTPRequestHandler):
+    suggested_json_regex = re.compile(r"^/json/suggested/[0-9]+$")
+    watched_json_regex = re.compile(r"^/json/watched/[0-9]+$")
     suggested_regex = re.compile(r"^/suggested/[0-9]+$")
     watched_regex = re.compile(r"^/watched/[0-9]+$")
+    static_regex = re.compile(r"^/static/.+$")
     movies_controller = MoviesController()
 
-    def set_headers(self, response_code: int = 200):
+    def set_json_headers(self, response_code: int = 200):
         self.send_response(response_code)
         self.send_header("Content-type", "application/json")
         self.end_headers()
@@ -46,27 +50,43 @@ class BotRequestHandler(BaseHTTPRequestHandler):
             )
         return movie_info
 
-    def get_suggested(self, server_id: int):
+    def get_suggested_json(self, server_id: int):
         suggested_movies = self.movies_controller.get_suggested_for_server(server_id)
         suggestion_list = []
         for suggestion in suggested_movies:
             movie_info = self.build_movie_base_info(suggestion)
             suggestion_list.append(movie_info)
 
-        self.set_headers()
+        self.set_json_headers()
         suggested = {"suggested": suggestion_list, "server_id": server_id}
         self.wfile.write(json.dumps(suggested).encode())
 
-    def get_watched(self, server_id: int):
+    def get_watched_json(self, server_id: int):
         watched_movies = self.movies_controller.get_watched_for_server(server_id)
         watched_list = []
         for watched in watched_movies:
             movie_info = self.build_movie_base_info(watched)
             watched_list.append(movie_info)
 
-        self.set_headers()
+        self.set_json_headers()
         suggested = {"watched": watched_list, "server_id": server_id}
         self.wfile.write(json.dumps(suggested).encode())
+
+    def serve_static(self, path: str):
+        static_parts = path.split("/")[2:]
+        static_path = Path(Path(__file__).parent, "webfiles", *static_parts)
+        logger.debug("static_parts: {}".format(static_parts))
+        logger.debug("static_path: {}".format(static_path))
+        if not static_path.exists():
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"")
+            return
+
+        with static_path.open("rb") as f:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(f.read())
 
     def get_server_id(self, path: str):
         return int(path.split("/")[-1])
@@ -74,18 +94,26 @@ class BotRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urlparse(self.path)
         path = parsed_path.path
-        if self.suggested_regex.match(path):
+        if self.static_regex.match(path):
+            self.serve_static(path)
+        elif self.suggested_json_regex.match(path):
+            server_id = self.get_server_id(path)
+            self.get_suggested_json(server_id)
+        elif self.watched_json_regex.match(path):
+            server_id = self.get_server_id(path)
+            self.get_watched_json(server_id)
+        elif self.suggested_regex.match(path):
             server_id = self.get_server_id(path)
             self.get_suggested(server_id)
         elif self.watched_regex.match(path):
             server_id = self.get_server_id(path)
             self.get_watched(server_id)
         else:
-            self.set_headers(404)
+            self.set_json_headers(404)
             self.wfile.write(b"")
 
     def do_HEAD(self):
-        self.set_headers()
+        self.set_json_headers()
 
 
 def run_webserver(port: int = 8000):
