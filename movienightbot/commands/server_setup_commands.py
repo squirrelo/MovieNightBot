f25c36b00032be040ee61bbc6a7689a125d51fda
Joshua Shorenstein <jshorens@gmail.com>
2023-10-04 10:37:47 -0600

    UTC only for movie time

--- movienightbot/commands/server_setup_commands.py
+++ movienightbot/commands/server_setup_commands.py
@@ -85,7 +85,7 @@ class ServerAdmin(app_commands.Group):
         for attr, value in server_row.__data__.items():
             if attr in ignore_attrs:
                 continue
-            if "time" in attr.lower():
+            if attr.lower() == "movie_time":
                 value = str(value) + " UTC"
             embed.add_field(name=attr, value=value, inline=False)
         return embed
