b8a40f31fc2f972638684870d7e7a0530aed87eb
Joshua Shorenstein <jshorens@gmail.com>
2026-04-03 16:35:17 -0600

    fix typing

--- movienightbot/commands/suggest.py
+++ movienightbot/commands/suggest.py
@@ -23,7 +23,7 @@ imdb_controller = IMDBInfoController()
 genre_controller = GenreController()
 
 
-def imdb_data(movie: str, kind: str) -> tuple[Union[None, IMDBInfo], Union[None, imdbinfo.MovieDetail]]:
+def imdb_data(movie: str, kind: str) -> tuple[Union[None, IMDBInfo], Union[None, imdbinfo.services.MovieDetail]]:
     suggestion = capitalize_movie_name(movie)
     imdb_info = get_imdb_info(suggestion, kind=kind)
     if not imdb_info:
